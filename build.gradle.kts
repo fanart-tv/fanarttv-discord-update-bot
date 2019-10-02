faba93193e9db900c88591a876da1916e584c1ec
Michael Haas <michael@michaelhaas.me>
2019-10-01 20:15:39 -0400

    NOGH: Add Retrofit Gson, correct JDA version, move all existing files

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
 
     implementation("com.squareup.retrofit2:retrofit:2.6.1")
-    implementation("net.dv8tion:JDA:4.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.1")
+    
+    implementation("net.dv8tion:JDA:4.0.0_50")
 }
 
 tasks.withType<KotlinCompile> {
