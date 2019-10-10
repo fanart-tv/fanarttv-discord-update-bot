77b8a1d013eab0671bf67042ce00b78f1b7253b0
Michael Haas <michael@michaelhaas.me>
2019-10-09 20:04:44 -0400

    NOGH: Add logging to bot

--- src/main/kotlin/tv/fanart/bot/UpdateBot.kt
+++ src/main/kotlin/tv/fanart/bot/UpdateBot.kt
@@ -1,11 +1,14 @@
 package tv.fanart.bot
 
+import klogging.KLoggerHolder
+import klogging.WithLogging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import tv.fanart.api.FanartApi
 import tv.fanart.api.model.ChangeResponse
 import tv.fanart.discord.ChangeMapper
 import tv.fanart.discord.DiscordWebhookClient
+import java.util.*
 
 class UpdateBot(
     private val fanartApi: FanartApi,
@@ -17,23 +20,31 @@ class UpdateBot(
         val mapped = changeMapper.mapChanges(changes)
         webhookClient?.sendCards(mapped)?.let { true } ?: false
     } catch (t: Throwable) {
+        logger.warn(t) { "Failed to send update webhook messages to Discord" }
         false
     }
 
     suspend fun update(lastUpdate: Long): Long? {
+        logger.debug { "Retrieving changes from Fanart API since ${Date(lastUpdate)}" }
         val activity = try {
             fanartApi.getChanges(after = lastUpdate)
         } catch (t: Throwable) {
+            logger.warn(t) { "Failed to retrieve changes from Fanart API" }
             null
         }
         return activity?.let {
             withContext(Dispatchers.IO) {
+                logger.debug { "Processing ${activity.changes.size} changes from Fanart API" }
                 if (processChanges(activity.changes)) {
-                    activity.currentTimestamp
+                    activity.currentTimestamp.also {
+                        logger.info { "Successfully sent messages for ${activity.changes.size} updates" }
+                    }
                 } else {
                     null
                 }
             }
         }
     }
+
+    companion object: WithLogging by KLoggerHolder()
 }
