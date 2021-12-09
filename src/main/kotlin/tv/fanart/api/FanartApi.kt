f411a44b9ec2de6ad67d498c2528c45b3021f5ef
KodeStar <kodestar@gmail.com>
2021-12-09 09:46:44 +0000

    add imports

--- src/main/kotlin/tv/fanart/api/FanartApi.kt
+++ src/main/kotlin/tv/fanart/api/FanartApi.kt
@@ -3,6 +3,8 @@ package tv.fanart.api
 import retrofit2.http.GET
 import retrofit2.http.Query
 import tv.fanart.api.model.ActivityResponse
+import tv.fanart.config.ConfigRepo
+import tv.fanart.config.model.UpdateConfig
 
 interface FanartApi {
     @GET(get<ConfigRepo>().updateConfig?.apiUrl ?: UpdateConfig.DEFAULT_API_URL)
